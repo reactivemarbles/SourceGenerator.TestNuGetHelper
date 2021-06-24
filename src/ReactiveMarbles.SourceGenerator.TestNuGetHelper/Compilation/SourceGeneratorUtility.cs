aa981a49eb462449460beaf6f8405cc66e670489
Glenn <5834289+glennawatson@users.noreply.github.com>
2021-06-24 16:56:00 +1000

    fix: Need to output driver for Verify (#5)

--- src/ReactiveMarbles.SourceGenerator.TestNuGetHelper/Compilation/SourceGeneratorUtility.cs
+++ src/ReactiveMarbles.SourceGenerator.TestNuGetHelper/Compilation/SourceGeneratorUtility.cs
@@ -71,16 +71,17 @@ namespace ReactiveMarbles.ObservableEvents.Tests
         /// <param name="compiler">The compiler.</param>
         /// <param name="compilationDiagnostics">The diagnostics which are produced from the compiler.</param>
         /// <param name="generatorDiagnostics">The diagnostics which are produced from the generator.</param>
+        /// <param name="generatorDriver">Output value for the driver.</param>
         /// <param name="sources">The source code files.</param>
         /// <returns>The source generator instance.</returns>
-        public T RunGenerator<T>(EventBuilderCompiler compiler, out ImmutableArray<Diagnostic> compilationDiagnostics, out ImmutableArray<Diagnostic> generatorDiagnostics, params string[] sources)
+        public T RunGenerator<T>(EventBuilderCompiler compiler, out ImmutableArray<Diagnostic> compilationDiagnostics, out ImmutableArray<Diagnostic> generatorDiagnostics, out GeneratorDriver generatorDriver, params string[] sources)
             where T : ISourceGenerator, new()
         {
             var compilation = CreateCompilation(compiler, sources);
 
             var generator = new T();
 
-            var newCompilation = RunGenerators(compilation, out generatorDiagnostics, new T());
+            var newCompilation = RunGenerators(compilation, out generatorDiagnostics, out generatorDriver, generator);
 
             compilationDiagnostics = newCompilation.GetDiagnostics();
 
@@ -125,16 +126,10 @@ namespace ReactiveMarbles.ObservableEvents.Tests
                 options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, deterministic: true));
         }
 
-        /// <summary>
-        /// Executes the source generators.
-        /// </summary>
-        /// <param name="compilation">The target compilation.</param>
-        /// <param name="diagnostics">The resulting diagnostics.</param>
-        /// <param name="generators">The generators to include in the compilation.</param>
-        /// <returns>The new compilation after the generators have executed.</returns>
-        private static Microsoft.CodeAnalysis.Compilation RunGenerators(Microsoft.CodeAnalysis.Compilation compilation, out ImmutableArray<Diagnostic> diagnostics, params ISourceGenerator[] generators)
+        private static Microsoft.CodeAnalysis.Compilation RunGenerators(Microsoft.CodeAnalysis.Compilation compilation, out ImmutableArray<Diagnostic> diagnostics, out GeneratorDriver generatorDriver, params ISourceGenerator[] generators)
         {
-            CreateDriver(compilation, generators).RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out diagnostics);
+            generatorDriver = CreateDriver(compilation, generators);
+            generatorDriver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out diagnostics);
             return outputCompilation;
         }
 
